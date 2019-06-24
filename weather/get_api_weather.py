876a132c9ad4196050ba28e9c7659fac9b875fc0
hyeongnam <morefly@naver.com>
2019-06-24 19:24:39 +0900

    weather-final

--- weather/get_api_weather.py
+++ weather/get_api_weather.py
@@ -2,6 +2,15 @@ import datetime
 import pytz
 import urllib.request
 import json
+import pymysql.cursors
+from decouple import config
+
+# connection = pymysql.connect(host='localhost',
+#                              user='root',
+#                              password='oracle',
+#                              db='weather',
+#                              charset='utf8',
+#                              cursorclass=pymysql.cursors.DictCursor)
 
 
 def get_api_date():
@@ -14,17 +23,18 @@ def get_api_date():
         if check_time < 2:
             day_calibrate = 1
             check_time = 23
-
+    if check_time < 1000:
+        check_time = '0' + str(check_time)
     date_now = datetime.datetime.now(tz=pytz.timezone('Asia/Seoul')).strftime('%Y%m%d')
+    date_day = datetime.datetime.now(tz=pytz.timezone('Asia/Seoul')).strftime('%m%d')
     check_date = int(date_now) - day_calibrate
-
     return (str(check_date), (str(check_time) + '00'))
 
 
 def get_weather_data():
     api_date, api_time = get_api_date()
     url = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?"
-    key = "serviceKey=2r07vW%2FaoLMCNlUxwTXR%2BDr0eevJKs7No3LxPU5ht2L34CLMrdiLqoYv4QbQ9rZvt75ltWsCftrR%2BoTW9nFM%2FQ%3D%3D"
+    key = f"serviceKey={config('SERVICE_KEY')}"
     date = "&base_date=" + api_date
     time = "&base_time=" + api_time
     nx = "&nx=97"
@@ -32,18 +42,20 @@ def get_weather_data():
     numOfRows = "&numOfRows=100"
     type = "&_type=json"
     api_url = url + key + date + time + nx + ny + numOfRows + type
-
+    print(api_url)
     data = urllib.request.urlopen(api_url).read().decode('utf8')
     data_json = json.loads(data)
 
     parsed_json = data_json['response']['body']['items']['item']
-
+    # 현재기준 날씨 추출
     target_date = parsed_json[0]['fcstDate']  # get date and time
     target_time = parsed_json[0]['fcstTime']
-
     date_calibrate = target_date  # date of TMX, TMN
     if target_time > '1300':
-        date_calibrate = str(int(target_date) + 1)
+        if str(target_date)[4:] == 1231:
+            pass
+        else:
+            date_calibrate = str(int(target_date) + 1)
 
     passing_data = {}
     for one_parsed in parsed_json:
@@ -59,3 +71,10 @@ def get_weather_data():
 
 if __name__ == '__main__':
     print(get_weather_data())
+    # try:
+    #     with connection.cursor() as cursor:
+    #         sql = "INSERT INTO WEATHER (name,price,yst,updn) VALUES (%s,%s,%s,%s)"
+    #         cursor.execute(sql,(name, price, yst, updn))
+    #     connection.commit()
+    # finally:
+    #     connection.close()
--- weather/weather.py
+++ weather/weather.py
@@ -1,19 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
+import operator
 
-url = 'https://weather.naver.com/rgn/townWetr.nhn?naverRgnCd=09680101'
-html = requests.get(url).text
+city = '서울시'
+dong = '종로구'
+input_weather = f'{city} {dong} 날씨'
+weather_url = f'https://search.naver.com/search.naver?query={input_weather}'
+html = requests.get(weather_url).text
 soup = BeautifulSoup(html, 'html.parser')
-date = soup.select('.w_now2 ul li h5')[0].text.strip()
-weather = soup.select('.w_now2 ul li em')[0].text.strip()
-weather_than0 = soup.select('.w_now2 ul li p .temp')[0].text.strip()
-weather_than = f'어제보다 {weather_than0}'
-rain0 = soup.select('.w_now2 ul li p')[0].text.split("|")[1].split("%")[0].strip()
-rain = f'{rain0}%'
-dust = soup.select('.w_now2 ul li p a span')[0].text.strip()
-dust_num = soup.select('#ly_atm dl dt span')[0].text.strip()
-print(date)
-print(weather)
-print(weather_than)
-print(rain)
-print(dust,dust_num)
+area = f'{city} {dong} 날씨입니다.'
+print(f'{city} {dong} 날씨입니다.')
+weather_temper = {}
+weather_rainfall = {}
+for i in range(8):
+    hour = soup.select('.list_area li')[i].select_one('.item_time').text.strip()
+    temper = soup.select('.list_area li')[i].select('.weather_item._dotWrapper')[0].text.strip()
+    rainfall = soup.select('.info_list.rainfall._tabContent .list_area li')[i].select_one('dl dd').text.strip()
+    if hour.split('시',1)[0] > '21':
+        break;
+    weather_temper[hour] = temper[:2]
+    weather_rainfall[hour] = rainfall.split('%',1)[0]
+wt_temper = sorted(weather_temper.items(), key=operator.itemgetter(1))
+print(f'오늘의 최저온도는 {wt_temper[0][1]}도, '
+      f'최고온도는 {wt_temper[-1][1]}도 입니다.')
+wt_rainfall = sorted(weather_rainfall.items(), key=operator.itemgetter(1))
+
+if wt_rainfall[-1][1] == '0':
+    print('오늘은 맑음입니다.')
+else:
+    print(f'오늘의 강수확률은 {wt_rainfall[-1][0]} {wt_rainfall[-1][1]} 입니다.')
+    if wt_rainfall[-1][1] >= '60':
+        print('우산준비해주세요!!')
+
+input_dust = f'{city} {dong} 미세먼지'
+dust_url = f'https://search.naver.com/search.naver?query={input_dust}'
+html1 = requests.get(dust_url).text
+soup1 = BeautifulSoup(html1, 'html.parser')
+dust = soup1.select('.state_info .figure')[0].text.strip()
+avr_dust = soup1.select('.state_info .figure')[1].text.strip()
+heavy_dust = soup1.select('.all_state .state_info .state')[0].text.strip()
+if 0 <= int(dust[0:2]) < 30:
+    dust_degree = '좋음'
+elif 30 <= int(dust[0:2]) < 80:
+    dust_degree = '보통'
+elif 80 <= int(dust[0:2]) < 150:
+    dust_degree = '나쁨'
+else:
+    dust_degree = '매우나쁨'
+if 0 <= int(heavy_dust[0:2]) < 15:
+    heavydust_degree = '좋음'
+elif 15 <= int(heavy_dust[0:2]) < 35:
+    heavydust_degree = '보통'
+elif 35 <= int(heavy_dust[0:2]) < 75:
+    heavydust_degree = '나쁨'
+else:
+    heavydust_degree = '매우나쁨'
+if 0 <= int(avr_dust[0:2]) < 30:
+    avr_dust_degree = '좋음'
+elif 30 <= int(avr_dust[0:2]) < 80:
+    avr_dust_degree = '보통'
+elif 80 <= int(avr_dust[0:2]) < 150:
+    avr_dust_degree = '나쁨'
+else:
+    avr_dust_degree = '매우나쁨'
+print(f'현재 미세먼지는: {dust} {dust_degree}, 초미세먼지는: {heavy_dust} {heavydust_degree}'
+      f'이고 1일 평균미세먼지는: {avr_dust} {avr_dust_degree}입니다.')
